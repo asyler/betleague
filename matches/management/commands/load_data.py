dd5c54ca126c7a2717f6d7cd0580e417e1ace131
asyler <vladomar@gmail.com>
2017-09-21 18:35:51 +0300

    Finish load data command

--- matches/management/commands/load_data.py
+++ matches/management/commands/load_data.py
@@ -1,10 +1,13 @@
 import csv
 from datetime import datetime
+from django.utils import timezone
 
+import re
 from django.contrib.auth.models import User
+from django.core.management import call_command
 from django.core.management.base import BaseCommand, CommandError
 
-from matches.models import Match, Bet
+from matches.models import Match, Bet, parse_score
 
 
 class Command(BaseCommand):
@@ -14,19 +17,37 @@ class Command(BaseCommand):
         parser.add_argument('dump_file', nargs=1, type=open)
 
     def handle(self, *args, **options):
+        call_command('flush','--noinput')
+        self.stdout.write(self.style.WARNING('Successfully flushed data'))
+
         f = options['dump_file'][0]
         data = csv.reader(f)
         usernames = next(data)[3:-1]
-        users = [User(username=username) for username in usernames]
+        users = [User.objects.create(username=username) for username in usernames]
         for row in data:
-            datetime_object = datetime.strptime('2017'+row[0]+row[1], '%Y%d.%m%H:%M')
+            datetime_object = timezone.make_aware(datetime.strptime('2017'+row[0]+row[1], '%Y%d.%m%H:%M'))
             home_team, away_team = row[2].split(' — ')
-            match = Match(
+            match = Match.objects.create(
                 datetime = datetime_object,
                 home_team = home_team,
-                away_team= away_team
+                away_team = away_team
             )
             for user, bet in zip(users,row[3:-1]):
-                bet = Bet(match = match, user=user)
-                bet.set_bet(bet)
+                bet_parsed = parse_score(bet)
+                if bet_parsed:
+                    home_score = bet_parsed[0]
+                    away_score = bet_parsed[1]
+                    bet_object = Bet(
+                        match_id = match.id,
+                        user=user,
+                        home_score=home_score,
+                        away_score=away_score
+                    )
+                    bet_object.save()
+
+            result = row[-1]
+            result_parsed = parse_score(result)
+            if result_parsed:
+                match.set_score(*result_parsed)
+
         self.stdout.write(self.style.SUCCESS('Successfully loaded data'))
