b1192bb8561404267a6d4cee1c5fec7ce872309e
asyler <vladomar@gmail.com>
2017-09-21 21:49:22 +0300

    Add basic password change

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
-from django.contrib.auth.views import logout
+from django.contrib.auth.views import logout, PasswordChangeView, PasswordChangeDoneView
 
 from . import views
 
 urlpatterns = [
     url(r'^login', views.login, name='login'),
+    url(r'^password_change', PasswordChangeView.as_view(), name='password_change'),
+    url(r'^password_change_done', PasswordChangeDoneView.as_view(), name='password_change_done'),
     url(r'^logout$', logout, {'next_page': '/'}, name='logout'),
 ]
